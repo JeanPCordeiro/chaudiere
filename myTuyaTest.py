808b74df710ecf305bbbb47f5ce2c410ec28efe0
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2023-03-13 16:01:08 +0000

    battery state

--- myTuyaTest.py
+++ myTuyaTest.py
@@ -11,4 +11,4 @@ c = tinytuya.Cloud(apiRegion=os.environ['TUYA_REGION'],apiKey=os.environ['TUYA_A
 devices = c.getdevices()
 for device in devices:
         result = c.getstatus(device['id'])
-        print ("Capteur %s Température = %2.1f°C Taux Humidité = %s%% Batterie = %s%%" % (device['name'],(result['result'][0]['value'])/10,result['result'][1]['value'],result['result'][3]['value']))
+        print ("Capteur %s \tTempérature = %2.1f°C Taux Humidité = %s%% Batterie = %s" % (device['name'],(result['result'][0]['value'])/10,result['result'][1]['value'],result['result'][2]['value']))
